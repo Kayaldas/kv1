8eed0c71f8d05309bfe3819477154ba3c118f301
Kayalvizhi Karuppiahdas <kayal241987@gmail.com>
2025-04-27 18:40:08 -0400

    intialcommit

---
+++ src/main/java/com/example/GitdemoSample1/GitdemoSample1Application.java
@@ -0,0 +1,20 @@
+package com.example.GitdemoSample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+public class GitdemoSample1Application {
+
+	public static void main(String[] args) {
+
+
+
+		SpringApplication.run(GitdemoSample1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GitdemoSample1/GitdemoSample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitdemoSample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitdemoSample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
